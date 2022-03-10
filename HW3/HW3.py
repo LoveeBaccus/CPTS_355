ec6ab6fcb2a7e1e43ffeec71b109a31d64a80de1
Lovee Baccus <58497392+LoveeBaccus@users.noreply.github.com>
2022-03-09 21:06:30 -0800

    Setting up HW3 in python

---
+++ HW3/HW3.py
@@ -0,0 +1,24 @@
+# CptS 355 - Spring 2022 - Assignment 3 - Python
+
+# Please include your name and the names of the students with whom you discussed any of the problems in this homework
+# Name:
+# Collaborators: 
+
+
+debugging = False
+def debug(*s): 
+     if debugging: 
+          print(*s)
+
+## problem 1 - all_games - 8%
+
+from functools import reduce
+## problem 2 - common_teams - 15%
+
+## problem 3 - get_wins - 16%
+
+## problem 4 - wins_by_year - 16%
+
+## problem 5 - longest_path - 16% 
+
+## problem 6 - counter - 20% 
