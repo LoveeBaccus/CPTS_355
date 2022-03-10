fcd35f8803064fb14de9eac7d94bdd2cf353df28
Lovee Baccus <58497392+LoveeBaccus@users.noreply.github.com>
2022-03-09 21:08:20 -0800

    Taking my grade prediction spreadsheets out of the git scope so I don't look like a fool

---
+++ HW3/HW3Tests.py
@@ -0,0 +1,48 @@
+#------------------------------------------------------
+#-- INCLUDE YOUR OWN TESTS IN THIS FILE
+#------------------------------------------------------
+import unittest
+from HW3 import *
+
+class HW3SampleTests(unittest.TestCase):
+    "Unittest setup file. Unittest framework will run this before every test."
+    def setUp(self):
+        pass
+    
+    #--- Problem 1----------------------------------
+    
+    def test_all_games(self):
+        pass
+        # Provide your own test here. Create your own input dictionary for this test (similar to self.wsu_games).
+    
+    #--- Problem 2----------------------------------
+    def test_common_teams(self):
+        pass
+        # Provide your own test here. Create your own input dictionary for this test (similar to self.my_cats_log). 
+        # You can re-use the data dictionary you created for all_games test.
+
+    #--- Problem 3 ----------------------------------
+    def test_get_wins(self):
+        pass
+        # Provide your own test here. Create your own input dictionary for this test (similar to self.wsu_games).
+        # You can re-use the data dictionary you created for all_games test.
+
+    #--- Problem 4----------------------------------
+    def test_wins_by_year(self):
+        pass
+        # Provide your own test here. Create your own input dictionary for this test (similar to self.wsu_games).
+        # You can re-use the data dictionary you created for all_games test.
+    
+    #--- Problem 5 ----------------------------------
+    def test_longest_path(self):
+        pass
+        # Provide your own test here. Create your own graph dictionary for this test (similar to self.graph). 
+
+    #--- Problem 6----------------------------------
+    def test_counter(self):
+        pass
+        # Provide your own test here. Initialize the iterator with your own input
+    
+if __name__ == '__main__':
+    unittest.main()
+
